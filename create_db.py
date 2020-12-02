b18b04a60f892583dd88eb230262f8505603e045
Bugato <49811854+Bugato@users.noreply.github.com>
2020-12-02 17:22:20 +0100

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,28 @@
+import sqlite3, config
+
+connection = sqlite3.connect(config.DB_PATH)
+
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock (
+        id INTEGER PRIMARY KEY, 
+        symbol TEXT NOT NULL UNIQUE, 
+        name TEXT NOT NULL
+    )
+""")
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock_price (
+        id INTEGER PRIMARY KEY, 
+        stock_id INTEGER,
+        date NOT NULL,
+        open NOT NULL, 
+        high NOT NULL, 
+        low NOT NULL, 
+        close NOT NULL, 
+        volume NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+""")
+
+connection.commit()
---
+++ populate_stocks.py
@@ -0,0 +1,24 @@
+import sqlite3, config
+import alpaca_trade_api as tradeapi
+
+connection = sqlite3.connect(config.DB_PATH)
+
+cursor = connection.cursor()
+connection.row_factory = sqlite3.Row
+
+cursor.execute("""SELECT symbol, name FROM stock""")
+
+rows = cursor.fetchall()
+symbols = [row[0] for row in rows]
+
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.URL)
+assets = api.list_assets()
+
+for asset in assets:
+    try:
+        if asset.status == "active" and asset.tradable:
+            cursor.execute("INSERT INTO stock (symbol, name) VALUES (?,?)", (asset.symbol, asset.name))
+    except Exception as e:
+        print(asset.symbol)
+        print(e)
+connection.commit()
