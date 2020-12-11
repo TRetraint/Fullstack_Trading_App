26e59a3414b1db485d5dc2c44a1ae5a2efe44804
Bugato <49811854+Bugato@users.noreply.github.com>
2020-12-12 00:51:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_KEY = "PKHVE2KPMCLPOK2TU59A"
-SECRET_KEY = "JQxscnDWQvJTwWbCv44ITM9fTI2v9gg84LKW1TTQ"
+API_KEY = "xxx"
+SECRET_KEY = "xxx"
 URL = "https://paper-api.alpaca.markets"
-DB_PATH = '/Users/Retraint/Desktop/Full_stack_Trading_app/app.db'
+DB_PATH = 'xxx'
