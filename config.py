8a2f3b5d09c7f666fcb25e5384195f31534a632d
Bugato <49811854+Bugato@users.noreply.github.com>
2020-12-14 01:41:11 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ API_KEY = "xxx"
 SECRET_KEY = "xxx"
 URL = "https://paper-api.alpaca.markets"
 DB_PATH = 'xxx'
+
+EMAIL_ADRESS = "xxx"
+EMAIL_PASSWORD = "xxx"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 465
+
+
